637dea23d8483fb375805ac48ba505368f166427
Jason Conger <jason@jasonconger.com>
2022-05-05 11:32:39 -0500

    Remove old build

--- TA-MS-AAD/README.txt
+++
@@ -1,4 +0,0 @@
-Microsoft Azure Add-on for Splunk
-
-Copyright (C) 2005-2022 Splunk Inc. All rights reserved.
-# Binary File Declaration
--- TA-MS-AAD/bin/import_declare_test.py
+++
@@ -1,8 +0,0 @@
-import os
-import pwd
-import sys
-
-import warnings
-warnings.filterwarnings("ignore")
-
-sys.path.insert(0, os.path.sep.join([os.path.dirname(os.path.realpath(os.path.dirname(__file__))), 'lib']))
